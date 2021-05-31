227b1f35bc78d69dd8f31f98c72e826fc3e4f3e8
echo <miyunniecho@gmail.com>
2021-05-30 22:25:51 -0700

    resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,7 +3,29 @@ import React from 'react';
 
 function Resume(){
     return(
-        <section></section>
+        <section id="resume">
+        <h1>RESUME</h1>
+        <h2>Links</h2>
+        <a href='' target="_blank" rel="noreferrer">
+            Resume Link
+        </a>
+        <h2>Front-end Proficiencies</h2>
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Javascript</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+        </ul>
+        <h2>Back-end Proficiencies</h2>
+        <ul>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MySql, Sequelize</li>
+            <li>MongoDB, Mongoose</li>
+            <li>GraphQL</li>
+        </ul>
+    </section>
     )
 }
 
