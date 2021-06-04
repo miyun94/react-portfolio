b855b978865459de5b72c8628bd490b861561699
echo <miyunniecho@gmail.com>
2021-06-03 22:56:13 -0700

    homepage route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import About from './components/About/index';
 import Nav from './components/Nav/index'; 
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 // import Homepage from './components/Homepage/index'; 
 //import Test from './components/Test page/index'; 
 import Contact from './components/Contact/index'; 
@@ -16,11 +16,11 @@ function App() {
 <main>
     <Switch>
       {/* <Route component = {About}/> */}
-      <Route path="/react-portfolio" component={About}/>
-      <Route exact path='/react-portfolio/about' component={ About }/>
-      <Route exact path='/react-portfolio/contact' component={ Contact}/>
-      <Route exact path='/react-portfolio/portfolio' component={Portfolio}/>
-      <Route exact path='/react-portfolio/resume' component={Resume}/>
+      <Route exact path="/react-portfolio"><Redirect to="/about"/><About/></Route>
+      <Route exact path='/about' component={ About }/>
+      <Route exact path='/contact' component={ Contact}/>
+      <Route exact path='/portfolio' component={Portfolio}/>
+      <Route exact path='/resume' component={Resume}/>
     </Switch>
 </main>
 </Router>
