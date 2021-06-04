efbb968cc8c6e235bd3e807a38ce59a0b6ea2664
echo <miyunniecho@gmail.com>
2021-06-03 22:48:05 -0700

    still figuring out routes

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ function App() {
 <main>
     <Switch>
       {/* <Route component = {About}/> */}
-      <Route exact path="/"><Redirect to "/about"/> </Route>
-      <Route exact path='/about' component={ About }/>
-      <Route exact path='/contact' component={ Contact}/>
-      <Route exact path='/portfolio' component={Portfolio}/>
-      <Route exact path='/resume' component={Resume}/>
+      <Route path="/react-portfolio" component={About}/>
+      <Route exact path='/react-portfolio/about' component={ About }/>
+      <Route exact path='/react-portfolio/contact' component={ Contact}/>
+      <Route exact path='/react-portfolio/portfolio' component={Portfolio}/>
+      <Route exact path='/react-portfolio/resume' component={Resume}/>
     </Switch>
 </main>
 </Router>
